83a554b4b7f7100f4bc9dd1036fc7d1bb697eb58
Sureshkumar <304624@cognizant.com>
2020-06-14 01:26:26 +0530

    cooking emp rev page

--- src/app/project-revenue/project-revenue.component.ts
+++ src/app/project-revenue/project-revenue.component.ts
@@ -11,7 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProjectRevenueComponent implements OnInit {
 
+  private empObj = [];
+  private empBaseDtls = "";
+  private empLeave = "";
+  private locHoliday = "";
+  private empSplWrk = "";
+  private locSplWrk = "";
+  private buffer = "";
   private revenue = [];
+  private dataReceived: Boolean = false;
   private currentYear = new Date().getFullYear();
   destroy$: Subject<boolean> = new Subject<boolean>();
 
@@ -23,13 +31,21 @@ export class ProjectRevenueComponent implements OnInit {
   ngOnInit() {
     let revenueYear = this.route.snapshot.paramMap.get('year');
     const esaId = this.route.snapshot.paramMap.get('esaId');
-    if (revenueYear === null || revenueYear === "") {
+    if (revenueYear === null || revenueYear === "" || revenueYear === undefined) {
       revenueYear = this.currentYear.toString();
     }
 
     this.projectService.projectRevenue(esaId, revenueYear).pipe(takeUntil(this.destroy$)).subscribe((data: any[]) => {
       console.log(data);
-      this.revenue = data;
+      this.empObj = data[0];
+      this.empBaseDtls = this.empObj[0];
+      this.empLeave = this.empObj[1].leaves;
+      this.locHoliday = this.empObj[2].publicHolidays;
+      this.empSplWrk = this.empObj[3].specialWorkDays.empSplWrk;
+      this.locSplWrk = this.empObj[3].specialWorkDays.locSplWrk;
+      this.buffer = this.empObj[4].buffers;
+      this.revenue = this.empObj[5].revenue;
+      this.dataReceived = true;
     });
   }
 
