4b3d3a4ef5308a4d4b8d7b0a3896d2e622df3e12
Prem <premkumar88pro@gmail.com>
2020-07-03 22:45:54 +0530

    add nav to proj of sel emp on home press

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -20,7 +20,7 @@ export class EmployeeDashboardComponent implements OnInit {
   private minMaxYear = [];
   private empList = [];
   private destroy$: Subject<Boolean> = new Subject<Boolean>();
-  private employeesAvailable: number = 0;
+  private availableEmployees: number = 0;
   private empObj = [];
   private empBaseDtl = [];
   private empLeave = [];
@@ -36,6 +36,8 @@ export class EmployeeDashboardComponent implements OnInit {
   private sowStop: string = "";
   private noRevForSelEmp: Boolean = false;
   private showEmployee = false;
+  private selectedProject: string = "";
+  private projectNotFound: Boolean = false;
 
   constructor(
     private employeeService: EmployeeService,
@@ -63,10 +65,6 @@ export class EmployeeDashboardComponent implements OnInit {
       }
     });
 
-    this.employeeService.getEmployee().pipe(takeUntil(this.destroy$)).subscribe((projectWorkforceDump: any[]) => {
-      this.empList = projectWorkforceDump;
-    });
-
     let yearParam = this.route.snapshot.paramMap.get('revenueYear');
     let employeeFilterParam = this.route.snapshot.paramMap.get('employeeFilter');
     if (yearParam === undefined || yearParam === null || yearParam.length !== 4) {
@@ -83,71 +81,84 @@ export class EmployeeDashboardComponent implements OnInit {
 
   }
 
-  public getEmployeeRevenue(selectedYear: string, selectedEmployee: string) {
-    if (selectedYear !== "" && selectedYear.trim() !== "Select Year" && selectedYear.length === 4) {
-      this.revenueYear = selectedYear;
-    } else if (selectedYear.trim() === "Select Year") {
+  public getEmployeeRevenue(forYear: string, forEmployee: string) {
+    if (forYear !== "" && forYear.trim() !== "Select Year" && forYear.length === 4) {
+      this.revenueYear = forYear;
+    } else if (forYear.trim() === "Select Year") {
       this.revenueYear = this.currentYear.toString();
     }
 
-    this.employeeService.getEmployee("", selectedYear).pipe(takeUntil(this.destroy$)).subscribe((projEmpListDump: any[]) => {
-      this.empList = projEmpListDump;
-      this.employeesAvailable = this.empList.length;
-    });
+    let splitStr = forEmployee.split("-");
+    if (splitStr[0].length === 0) {
+      this.projectNotFound = true;
+    }
+    this.selectedProject = splitStr[0];
 
-    this.employeeService.employeeRevenue(selectedYear, selectedEmployee).pipe(takeUntil(this.destroy$)).subscribe((employeeRevenueDump: any[]) => {
-      if (employeeRevenueDump.length > 1) {
-        this.empObj = employeeRevenueDump;
-        this.empBaseDtl = this.empObj[0];
-        this.empLeave = this.empObj[1].leaves;
-        this.locHoliday = this.empObj[2].publicHolidays;
-        this.empSplWrk = this.empObj[3].specialWorkDays.empSplWrk;
-        this.locSplWrk = this.empObj[3].specialWorkDays.locSplWrk;
-        this.buffer = this.empObj[4].buffers;
-        this.revenue = this.empObj[5].revenue;
-        this.totalRevenue = 0;
-        this.totalCmiRevenue = 0;
-        this.sowForeCast = "";
-        this.revenueYear = selectedYear;
-
-        let sowDate = this.empBaseDtl["sowStart"];
-        let splitStr = sowDate.split("-");
-        this.sowStart = splitStr[1] + "-" + splitStr[2];
-
-        sowDate = this.empBaseDtl["sowStop"];
-        splitStr = sowDate.split("-");
-        this.sowStop = splitStr[1] + "-" + splitStr[2];
-
-        sowDate = this.empBaseDtl["foreseenSowStop"];
-        if (sowDate.length > 0) {
-          splitStr = sowDate.split("-");
-          this.sowForeCast = splitStr[1] + "-" + splitStr[2];
+    this.employeeService.getEmployee("", this.revenueYear).pipe(takeUntil(this.destroy$)).subscribe((projectWorkforceDump: any[]) => {
+      this.empList = projectWorkforceDump;
+      projectWorkforceDump.forEach((projectEmployee) => {
+        if (this.selectedProject === projectEmployee["_id"].toString()) {
+          this.availableEmployees = projectEmployee.workforce.length;
         }
+      });
 
-        let lSowStart = new Date(this.empBaseDtl["sowStart"]);
-        let lSowStop = new Date(this.empBaseDtl["sowStop"]);
-        let lSowForeCast = new Date(this.empBaseDtl["sowStop"]);
-        if (this.empBaseDtl["foreseenSowStop"].length > 0) {
-          lSowForeCast = new Date(this.empBaseDtl["foreseenSowStop"]);
-        }
+      if (this.availableEmployees > 0) {
+        this.employeeService.employeeRevenue(this.revenueYear, forEmployee).pipe(takeUntil(this.destroy$)).subscribe((employeeRevenueDump: any[]) => {
+          if (employeeRevenueDump.length > 1) {
+            this.empObj = employeeRevenueDump;
+            this.empBaseDtl = this.empObj[0];
+            this.empLeave = this.empObj[1].leaves;
+            this.locHoliday = this.empObj[2].publicHolidays;
+            this.empSplWrk = this.empObj[3].specialWorkDays.empSplWrk;
+            this.locSplWrk = this.empObj[3].specialWorkDays.locSplWrk;
+            this.buffer = this.empObj[4].buffers;
+            this.revenue = this.empObj[5].revenue;
+            this.totalRevenue = 0;
+            this.totalCmiRevenue = 0;
+            this.sowForeCast = "";
 
-        let iRevenueYear = parseInt(this.revenueYear, 10);
-        if (iRevenueYear < lSowStart.getFullYear() || (iRevenueYear > lSowStop.getFullYear() && iRevenueYear > lSowForeCast.getFullYear())) {
-          this.noRevForSelEmp = true;
-        } else {
-          this.noRevForSelEmp = false;
-        }
+            let sowDate = this.empBaseDtl["sowStart"];
+            let splitStr = sowDate.split("-");
+            this.sowStart = splitStr[1] + "-" + splitStr[2];
 
-        this.revenue.forEach((revenueObj) => {
-          this.totalRevenue += revenueObj.revenueAmount;
-          this.totalCmiRevenue += revenueObj.cmiRevenueAmount;
+            sowDate = this.empBaseDtl["sowStop"];
+            splitStr = sowDate.split("-");
+            this.sowStop = splitStr[1] + "-" + splitStr[2];
+
+            sowDate = this.empBaseDtl["foreseenSowStop"];
+            if (sowDate.length > 0) {
+              splitStr = sowDate.split("-");
+              this.sowForeCast = splitStr[1] + "-" + splitStr[2];
+            }
+
+            let lSowStart = new Date(this.empBaseDtl["sowStart"]);
+            let lSowStop = new Date(this.empBaseDtl["sowStop"]);
+            let lSowForeCast = new Date(this.empBaseDtl["sowStop"]);
+            if (this.empBaseDtl["foreseenSowStop"].length > 0) {
+              lSowForeCast = new Date(this.empBaseDtl["foreseenSowStop"]);
+            }
+
+            let iRevenueYear = parseInt(this.revenueYear, 10);
+            if (iRevenueYear < lSowStart.getFullYear() || (iRevenueYear > lSowStop.getFullYear() && iRevenueYear > lSowForeCast.getFullYear())) {
+              this.noRevForSelEmp = true;
+            } else {
+              this.noRevForSelEmp = false;
+            }
+
+            this.revenue.forEach((revenueObj) => {
+              this.totalRevenue += revenueObj.revenueAmount;
+              this.totalCmiRevenue += revenueObj.cmiRevenueAmount;
+            });
+          }
         });
+        this.router.navigate(['/employeeRevenue', this.revenueYear, forEmployee]);
       }
     });
   }
 
   public goToHome(): void {
-    this.router.navigate(['/dashboard', this.revenueYear]);
+    console.log("going to " + this.selectedProject + " project for year " + this.revenueYear);
+    this.router.navigate(['/dashboard', this.revenueYear, this.selectedProject]);
   }
 
   public employeeMonthDetailView(monthIndex: string) {
